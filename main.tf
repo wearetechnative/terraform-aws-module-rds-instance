1d5891da7a9c8d6eeb0b35240599ef01c74547dd
Randall de Boer <129743268+randalldeboer@users.noreply.github.com>
2024-09-24 22:43:28 +0200

    fix for performance insights

--- main.tf
+++ main.tf
@@ -56,10 +56,12 @@ resource "aws_db_instance" "this" {
   //performance_insights_enabled = var.performance_insights_enabled != null ? var.performance_insights_enabled : local.performance_insights_available
   performance_insights_enabled = var.performance_insights_enabled
   performance_insights_kms_key_id = var.kms_key_arn
+  performance_insights_retention_period = var.performance_insights_enabled == "true" ? 731 : 7
+
   //performance_insights_retention_period = var.performance_insights_enabled != null ? var.performance_insights_enabled : var.performance_insights_retention_period
   //performance_insights_kms_key_id       = local.performance_insights_available ? var.kms_key_arn : null
-  performance_insights_retention_period = var.performance_insights_enabled ? 731 : 7 ? 0 : 1 # either 731 (2 years) or 7 days... 
-
+  //performance_insights_retention_period = var.performance_insights_enabled ? 731 : 7 ? 0 : 1 # either 731 (2 years) or 7 days... 
+  //performance_insights_retention_period = var.performance_insights_enabled && var.performance_insights_retention_period ? 731 : 7 ? 0 : 1
   # keep default
   # port =
 
