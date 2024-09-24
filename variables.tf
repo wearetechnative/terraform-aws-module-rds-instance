c44ba8a6c91dfd277620d9bbd1668db9a4e8bbc6
Randall de Boer <129743268+randalldeboer@users.noreply.github.com>
2024-09-24 12:50:10 +0200

    fix for performance insights

--- variables.tf
+++ variables.tf
@@ -172,3 +172,9 @@ variable "maintenance_window" {
   description = "maintenance window for rds instance updates"
   default = "Sun:02:00-Sun:03:00"
 }
+
+variable "var.performance_insights_enabled" {
+  description = "performance insights"
+  default     = ""
+}
+
