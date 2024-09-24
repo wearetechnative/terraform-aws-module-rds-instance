7509bb71819ddcd3ca4b294f65689a3d5888c734
Randall de Boer <129743268+randalldeboer@users.noreply.github.com>
2024-09-24 23:14:22 +0200

    fix for performance insights

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_db_instance" "this" {
   //performance_insights_enabled = var.performance_insights_enabled != null ? var.performance_insights_enabled : local.performance_insights_available
   performance_insights_enabled = var.performance_insights_enabled
   performance_insights_kms_key_id = var.kms_key_arn
-  performance_insights_retention_period = var.performance_insights_enabled == "true" ? 731 : false
+  performance_insights_retention_period = var.performance_insights_enabled == "true" ? 731 : null
 
   //performance_insights_retention_period = var.performance_insights_enabled != null ? var.performance_insights_enabled : var.performance_insights_retention_period
   //performance_insights_kms_key_id       = local.performance_insights_available ? var.kms_key_arn : null
