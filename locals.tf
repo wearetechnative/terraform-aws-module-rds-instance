b52342e0afa685371def75832956c313a7cb43cf
Randall de Boer <129743268+randalldeboer@users.noreply.github.com>
2024-09-24 13:51:36 +0200

    fix for performance insights

--- locals.tf
+++ locals.tf
@@ -1,6 +1,3 @@
 locals {
-  performance_insights_available = (
-    (var.instance_class == "db.t4g.micro" && var.engine == "mariadb" && var.engine_major_version == "10.5")
-    || (var.instance_class == "db.t2.small" && var.engine == "mysql" && var.engine_major_version == "5.7")
-  ? false : true)
+  performance_insights_available = ""
 }
